638436b8c4abeefbc39c0e4512a594379309a0fe
Itumeleng <fortuneismaname@gmail.com>
2020-10-08 16:24:56 +0200

    My First Ionic API Commit

---
+++ src/app/tab1/tab1.page.ts
@@ -0,0 +1,23 @@
+  
+import { Component } from '@angular/core';
+import { CovidServiceService } from '../covid-service.service';
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: './tab1.page.html',
+  styleUrls: ['./tab1.page.scss'],
+})
+export class Tab1Page {
+
+  info: any = null;
+
+  constructor(private covidService: CovidServiceService) { 
+
+    this.covidService.getAll().subscribe((data)=>{
+      this.info = data;
+    });
+
+  }
+
+
+}
---
+++ src/app/tab2/tab2.page.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { CovidServiceService } from '../covid-service.service';
+
+
+@Component({
+  selector: 'app-tab2',
+  templateUrl: './tab2.page.html',
+  styleUrls: ['./tab2.page.scss'],
+})
+export class Tab2Page  {
+  countries: any = null;
+  searchCountry: any;
+
+  constructor(private covidService: CovidServiceService) { 
+      
+    this.covidService.getCountries().subscribe((data)=>{
+      this.countries = data;
+    });
+
+  }
+
+}
